e52e3d989989654e55822a53a238680733967acc
yogireddy21 <117932234+yogireddy21@users.noreply.github.com>
2024-02-02 21:55:40 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import SingIn from './pages/SingIn'
+import SignUp from './pages/SignUp'
+import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
+import CreateListing from './pages/CreateListing'
+import UpdateListing from './pages/UpdateListing'
+import Listing from './pages/Listing'
+import Search from './pages/Search'
+
+export default function App() {
+  return <BrowserRouter>
+  <Header/>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/sign-in" element={<SingIn/>}/>
+    <Route path="/sign-up" element={<SignUp/>}/>
+    <Route path="/listing/:listingId" element={<Listing/>}/>
+    
+    <Route  element={<PrivateRoute/>}>
+    <Route path="/profile" element={<Profile/>}/>
+    <Route path="/create-listing" element={<CreateListing/>}/>
+    <Route path="/update-listing/:listingId" element={<UpdateListing/>}/>
+    </Route>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/search" element ={<Search/>}/>
+
+  </Routes>
+  </BrowserRouter>
+}
---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyATWOvZ1gDVyvS7CYhT-XyYabUHIZUlkyk",
+  authDomain: "estate123-c40cb.firebaseapp.com",
+  projectId: "estate123-c40cb",
+  storageBucket: "estate123-c40cb.appspot.com",
+  messagingSenderId: "332085355300",
+  appId: "1:332085355300:web:62cb725b200431642649b1",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
